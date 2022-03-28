26b360fdae71dc3af696d98365283872a5b25a00
limmi26 <kisamakiazuel@gmail.com>
2022-03-28 16:17:21 +0700

    perubahan 5

---
+++ lifebook-frontend/src/pages/Homepage.js
@@ -0,0 +1,15 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+export default function Homepage() {
+    return(
+        <div>
+            <Header />
+            <main>
+                <h1>ini Homepage</h1>
+            </main>
+            <Footer />
+        </div>
+        
+    )
+}
